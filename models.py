da280520de4aba03110bacad655977787e5d4947
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-01 14:07:17 +0700

    fix: remove first layer cnn proposed & freeze vgg features

--- models.py
+++ models.py
@@ -9,34 +9,36 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
         # First layer
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=64, kernel_size=3, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU()
 
         # Second layer
         self.conv2 = nn.Conv2d(
-            in_channels=32, out_channels=64, kernel_size=3, padding=1
+            in_channels=64, out_channels=128, kernel_size=3, padding=1
         )
-        self.bn2 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
         self.relu2 = nn.ReLU()
 
         # Third layer
         self.conv3 = nn.Conv2d(
-            in_channels=64, out_channels=128, kernel_size=3, padding=1
+            in_channels=128, out_channels=128, kernel_size=3, padding=1
         )
         self.bn3 = nn.BatchNorm2d(128)
         self.relu3 = nn.ReLU()
 
         # Fourth layer
         self.conv4 = nn.Conv2d(
-            in_channels=128, out_channels=128, kernel_size=3, padding=1
+            in_channels=128, out_channels=256, kernel_size=3, padding=1
         )
-        self.bn4 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
         self.relu4 = nn.ReLU()
 
         # Fifth layer
         self.conv5 = nn.Conv2d(
-            in_channels=128, out_channels=256, kernel_size=3, padding=1
+            in_channels=256, out_channels=256, kernel_size=3, padding=1
         )
         self.bn5 = nn.BatchNorm2d(256)
         self.relu5 = nn.ReLU()
@@ -47,13 +49,6 @@ class CNNModel(nn.Module):
         )
         self.bn6 = nn.BatchNorm2d(256)
         self.relu6 = nn.ReLU()
-
-        # Seventh layer
-        self.conv7 = nn.Conv2d(
-            in_channels=256, out_channels=256, kernel_size=3, padding=1
-        )
-        self.bn7 = nn.BatchNorm2d(256)
-        self.relu7 = nn.ReLU()
 
         # Flatten layer
         self.flatten = nn.Flatten()
@@ -68,21 +63,21 @@ class CNNModel(nn.Module):
     def forward_conv(self, x):
         helper.print_current_memory_usage("conv1", before=True)
         x = self.relu1(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+        helper.print_current_memory_usage("conv")
+
         x = self.relu2(self.bn2(self.conv2(x)))
-        x = F.max_pool2d(x, 2)
-        helper.print_current_memory_usage("conv1-2")
-
         x = self.relu3(self.bn3(self.conv3(x)))
+        x = F.max_pool2d(x, 2)
+        helper.print_current_memory_usage("conv2-3")
+
         x = self.relu4(self.bn4(self.conv4(x)))
-        x = F.max_pool2d(x, 2)
-        helper.print_current_memory_usage("conv3-4")
-
         x = self.relu5(self.bn5(self.conv5(x)))
-        x = self.relu6(self.bn6(self.conv6(x)))
         x = F.max_pool2d(x, 2)
-        helper.print_current_memory_usage("conv5-6")
+        helper.print_current_memory_usage("conv4-5")
 
-        x = self.relu7(self.bn7(self.conv7(x)))
+        x = self.relu6(self.bn6(self.conv6(x)))
+        helper.print_current_memory_usage("conv5-6")
 
         return x
 
@@ -113,6 +108,9 @@ class VGGModel(nn.Module):
     def __init__(self):
         super(VGGModel, self).__init__()
         self.vgg16 = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+
+        for param in self.vgg16.features.parameters():
+            param.requires_grad = False
 
         self.vgg16.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
