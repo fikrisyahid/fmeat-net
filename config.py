159f17b341b1bc9f0853bcb40b6c524559954ca3
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-10-31 09:02:41 +0700

    config: add training combination

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import torch
 
 # GENERAL CONFIGS
-MODEL_USED = "CNN"  # MODEL_USED should be either "CNN" or "VGG"
+MODEL_USED = "VGG"  # MODEL_USED should be either "CNN" or "VGG"
 
 # HYPERPARAMETER CONFIGS
 DATASET_DIR = "./dataset"
-EPOCH_AMOUNT = 30
+EPOCH_AMOUNT = 1
 DROPOUT_RATE = 0.8
 LEARNING_RATE = 0.0001
 BATCH_SIZE = 32
@@ -22,8 +22,8 @@ MIXED_PRECISION_MODE_COLLECTION = {
     0: (torch.float32, torch.float32),
     1: (torch.float16, torch.float32),
     2: (torch.float64, torch.float64),  # Slower, cuDNN does not support float64
-    # 3: (torch.float16, torch.float64), Not supported by autocast, same as number 2
-    # 4: (torch.float32, torch.float64), Not supported by autocast, same as number 2
+    # 3: (torch.float16, torch.float64), Not supported by autocast
+    # 4: (torch.float32, torch.float64), Not supported by autocast
 }
 CONV_DTYPE, FC_DTYPE = MIXED_PRECISION_MODE_COLLECTION.get(
     MIXED_PRECISION_MODE, (torch.float32, torch.float32)
@@ -32,3 +32,66 @@ CONV_DTYPE, FC_DTYPE = MIXED_PRECISION_MODE_COLLECTION.get(
 # LOG CONFIGS
 PRINT_MEMORY_USAGE = False
 SUMMARY_DISPLAYED = MIXED_PRECISION_MODE == 0 or MIXED_PRECISION_MODE == 1
+
+TRAINING_COMBINATION = [
+    {
+        "model_type": "CNN",
+        "pso": False,
+        "mp_mode": 0,
+    },
+    {
+        "model_type": "CNN",
+        "pso": True,
+        "mp_mode": 0,
+    },
+    {
+        "model_type": "CNN",
+        "pso": False,
+        "mp_mode": 1,
+    },
+    {
+        "model_type": "CNN",
+        "pso": False,
+        "mp_mode": 2,
+    },
+    {
+        "model_type": "CNN",
+        "pso": True,
+        "mp_mode": 1,
+    },
+    {
+        "model_type": "CNN",
+        "pso": True,
+        "mp_mode": 2,
+    },
+    {
+        "model_type": "VGG",
+        "pso": False,
+        "mp_mode": 0,
+    },
+    {
+        "model_type": "VGG",
+        "pso": True,
+        "mp_mode": 0,
+    },
+    {
+        "model_type": "VGG",
+        "pso": False,
+        "mp_mode": 1,
+    },
+    {
+        "model_type": "VGG",
+        "pso": False,
+        "mp_mode": 2,
+    },
+    {
+        "model_type": "VGG",
+        "pso": True,
+        "mp_mode": 1,
+    },
+    {
+        "model_type": "VGG",
+        "pso": True,
+        "mp_mode": 2,
+    },
+]
