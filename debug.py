8df294bc5b2f4c181729a476a7c78eeaaa13e476
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-11-01 14:14:27 +0700

    debug: update testing

--- debug.py
+++ debug.py
@@ -1,3 +1,9 @@
 import helper
 
-helper.visualize_augmentations("./dataset/training/sapi/s1.jpg")
+helper.visualize_augmentations("./dataset/training/sapi/s7.jpg")
+# helper.generate_augmented_images(source_dir="./dataset/training", destination_dir="./dataset/augmented/training")
+# helper.generate_augmented_images(source_dir="./dataset/validation", destination_dir="./dataset/augmented/validation")
+# helper.generate_augmented_images(source_dir="./dataset/testing", destination_dir="./dataset/augmented/testing")
+# print(helper.get_normalization_mean_std("./dataset/augmented/training"))
+# print(helper.get_normalization_mean_std("./dataset/augmented/validation"))
+# print(helper.get_normalization_mean_std("./dataset/augmented/testing"))
